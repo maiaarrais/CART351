d4d4b947c892255c6c7bb54d7c03edb5c4be8d8b
maiaarrais <maiacarrais@gmail.com>
2025-10-21 08:16:31 -0400

    exercise 2

--- Class Exercises /week 7/main.py
+++ Class Exercises /week 7/main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 app = Flask(__name__)
 
 @app.route('/')
@@ -10,7 +10,6 @@ def index():
     passedDictionary={"fav_color":"fuscia", "fav_veg":"cauliflower","fav_fruit":"kiwi", "fav_animal":"toucan"}
     return render_template("index.html", user={"username":"maia"}, passedDictionary = passedDictionary, imgPath="img1.jpg")
 
-
 @app.route('/pineParent')
 def pineParent():
     return render_template("pineAppleParent.html")
@@ -19,6 +18,20 @@ def pineParent():
 def about():
     return render_template("pineAppleChild.html", 
                            dataPassedA = "child A!")
+
+
+
+@app.route("/addPlantData")
+def addPlantData():
+    return render_template("addPlantData.html")
+
+@app.route("/thank_you")
+def thank_you():
+    app.logger.info(request.args)
+    return render_template("thankyou.html", owner_name=request.args["o_name"])
+
+
+
 
 
 app.run(debug=True)
--- maiaArrais_exercise_2/exercise-two.py
+++ maiaArrais_exercise_2/exercise-two.py
@@ -1,4 +1,4 @@
-from flask import Flask,render_template,request
+from flask import Flask,render_template, request
 import os
 app = Flask(__name__)
 
@@ -6,36 +6,54 @@ app = Flask(__name__)
 # the default route
 @app.route("/")
 def index():
-      return render_template("index.html")
+      return render_template("base.html")
 
 #*************************************************
 
 #Task: Variables and JinJa Templates
-# @app.route("/t1")
-# def t1():
-#       the_topic = "donuts"
-#       number_of_donuts = 28
-#       donut_data= {
-#       "flavours":["Regular", "Chocolate", "Blueberry", "Devil's Food"],
-#       "toppings": ["None","Glazed","Sugar","Powdered Sugar",
-#                    "Chocolate with Sprinkles","Chocolate","Maple"]
-#                    }
+@app.route("/t1")
+def t1():
+      the_topic = "donuts"
+      number_of_donuts = 28
+      donut_data= {
+      "flavours":["Regular", "Chocolate", "Blueberry", "Devil's Food"],
+      "toppings": ["None","Glazed","Sugar","Powdered Sugar",
+                   "Chocolate with Sprinkles","Chocolate","Maple"]
+                   }
       
-#       icecream_flavors = ["Vanilla","Raspberry","Cherry", "Lemon"]
-#       return render_template("t1.html")
+      products = [
+        {"flavour": "Regular",      "img": "donut_sprinkles.png"},
+        {"flavour": "Chocolate",    "img": "donut_a.png"},
+        {"flavour": "Blueberry",    "img": "donut_e.png"},
+        {"flavour": "Devil's Food", "img": "donut_b.png"},
+    ]
+      
+      icecream_flavors = ["Vanilla","Raspberry","Cherry", "Lemon"]
+      return render_template("t1.html", topic=the_topic, num_donuts=number_of_donuts, flavours = donut_data["flavours"], toppings = donut_data["toppings"], icecreams = icecream_flavors, products = products)
 
 #*************************************************
 
-#Task: HTML Form get & Data 
-# @app.route("/t2")
-# def t2():
-#     return render_template("t2.html")
+# Task: HTML Form get & Data 
+@app.route("/t2")
+def t2():
+    return render_template("t2.html")
 
-# @app.route("/thank_you_t2")
-# def thank_you_t2():
-#     return render_template("thankyou_t2.html")
+@app.route("/thank_you_t2")
+def thank_you_t2():
+    # Grab GET params (names must match the form fields)
+    first = request.args.get("first", "").strip()
+    second = request.args.get("second", "").strip()
+    notes = request.args.get("notes", "").strip()
+
+    # Combine into one long string
+    combined = f"{first} {second} {notes}".strip()
+
+    # Replace vowels with asterisks (case-insensitive)
+    import re
+    modified = re.sub(r"[aeiouAEIOU]", "*", combined)
+
+    # Pass to the template
+    return render_template("thankyou_t2.html", combined=combined, result=modified)
 
 #*************************************************
-
-#run
 app.run(debug=True)
